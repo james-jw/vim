bb184f2f14eae57853fe088e02819abc193f3ff3
James Wright <james.jw@hotmail.com>
2015-06-13 12:30:36 -0600

    Reorded git clones to have longest be last.

--- install-vim-config.sh
+++ install-vim-config.sh
@@ -1,11 +1,16 @@
 mkdir autoload
 mkdir bundle
-cd autoload
-wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim --no-check-certificate
-cd ..
-git clone --depth=1 git@github.com:scrooloose/nerdtree.git bundle/nerdtree.vim
-git clone --depth=1 https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+wget -nv -N https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim --no-check-certificate autoload/pathogen.vim 
+wget -nv -N https://raw.githubusercontent.com/james-jw/vim/master/vimrc vimrc --no-check-certificate
+
+wget -nv -N https://conque.googlecode.com/files/conque_2.3.zip --no-check-certificate
+unzip conque_2.3.zip bundle/
+rm conque_2.3.zip
+
 git clone --depth=1 git@github.com:tpope/vim-surround.git bundle/vim-surround.vim
 git clone --depth=1 git@github.com:jeroenp/vim-xquery-syntax.git bundle/xquery-syntax.vim
 git clone --depth=1 git@github.com:james-jw/vim-autoclose.git bundle/autoclose.vim
-wget https://raw.githubusercontent.com/james-jw/vim/master/vimrc --no-check-certificate
+git clone --depth=1 git@github.com:scrooloose/nerdtree.git bundle/nerdtree.vim
+git clone --depth=1 https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+
+printf 'Vim configuration complete!'
