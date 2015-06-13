7d8cefd87854778b9e5888c8088bcc2034eb675b
James Wright <james.jw@hotmail.com>
2015-06-13 11:26:18 -0600

    Added flag to ignore invalid certificates

--- install-vim-config.sh
+++ install-vim-config.sh
@@ -1,11 +1,11 @@
 mkdir autoload
 mkdir bundle
 cd autoload
-wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim --no-check-certificate
 cd ..
 git clone --depth=1 git@github.com:scrooloose/nerdtree.git bundle/nerdtree.vim
 git clone --depth=1 https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
 git clone --depth=1 git@github.com:tpope/vim-surround.git bundle/vim-surround.vim
 git clone --depth=1 git@github.com:jeroenp/vim-xquery-syntax.git bundle/xquery-syntax.vim
 git clone --depth=1 git@github.com:james-jw/vim-autoclose.git bundle/autoclose.vim
-wget https://raw.githubusercontent.com/james-jw/vim/master/vimrc
+wget https://raw.githubusercontent.com/james-jw/vim/master/vimrc --no-check-certificate
