72f247a2864bcb9ff8b101a0d7b6c4bc3038b0b1
James Wright <james.jw@hotmail.com>
2015-06-13 11:15:53 -0600

    Added wget to pull latest vimrc from my repo

--- install-vim-config.sh
+++ install-vim-config.sh
@@ -8,3 +8,4 @@ git clone --depth=1 https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
 git clone --depth=1 git@github.com:tpope/vim-surround.git bundle/vim-surround.vim
 git clone --depth=1 git@github.com:jeroenp/vim-xquery-syntax.git bundle/xquery-syntax.vim
 git clone --depth=1 git@github.com:james-jw/vim-autoclose.git bundle/autoclose.vim
+wget https://raw.githubusercontent.com/james-jw/vim/master/vimrc
