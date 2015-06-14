c13870b10801a9b71919f63625d7f3924441216f
James Wright <james.jw@hotmail.com>
2015-06-14 15:30:11 -0600

    Removed autoclose plugin

--- install-vim-config.sh
+++ install-vim-config.sh
@@ -6,8 +6,7 @@ cd ..
 wget -nv -N https://raw.githubusercontent.com/james-jw/vim/master/vimrc --no-check-certificate
 
 git clone --depth=1 git@github.com:tpope/vim-surround.git bundle/vim-surround.vim
-git clone --depth=1 git@github.com:jeroenp/vim-xquery-syntax.git bundle/xquery-syntax.vim
-git clone --depth=1 git@github.com:james-jw/vim-autoclose.git bundle/autoclose.vim 
+git clone --depth=1 git@github.com:james-jw/vim-xquery-syntax.git bundle/xquery-syntax.vim
 git clone --depth=1 git@github.com:scrooloose/nerdtree.git bundle/nerdtree.vim
 git clone --depth=1 git@github.com:kien/ctrlp.vim.git bundle/ctrlp.vim
 
