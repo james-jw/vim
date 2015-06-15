ab05df8e5e5d2074b190da675f1412a5959c5bc5
James Wright <james.jw@hotmail.com>
2015-06-15 09:33:51 -0600

    Fixed YouCompleteMe install

--- install-vim-config.sh
+++ install-vim-config.sh
@@ -8,9 +8,6 @@ cd autoload/
 wget -nv -N https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim --no-check-certificate
 cd ..
 wget -nv -N https://raw.githubusercontent.com/james-jw/vim/master/vimrc --no-check-certificate
-wget -nv -N https://bitbucket.org/ns9tks/vim-autocomplpop/get/tip.zip --no-check-certificate
-unzip tip.zip -d bundle/autocomplete.vim
-rm tip.zip
 
 git clone --depth=1 git@github.com:tpope/vim-surround.git bundle/vim-surround.vim
 git clone --depth=1 git@github.com:james-jw/vim-xquery-syntax.git bundle/xquery-syntax.vim
@@ -21,11 +18,16 @@ git clone --depth=1 git@github.com:scrooloose/nerdtree.git bundle/nerdtree.vim
 git clone --depth=1 git@github.com:tpope/vim-fugitive.git bundle/fugitive.vim
 git clone --depth=1 git@github.com:kien/ctrlp.vim.git bundle/ctrlp.vim
 git clone --depth=1 git@github.com:james-jw/xqueryvim.git bundle/xqueryvim.vim
-git clone --depth=1 git@github.com:Valloric/YouCompleteMe.git bundle/YouCompleteMe.vim
 git clone --depth=1 git@github.com:aquach/vim-http-client.git bundle/http-client.vim
 
+# Install You Complete Me
+git clone --depth=1 git@github.com:Valloric/YouCompleteMe.git bundle/YouCompleteMe.vim
+sudo apt-get install build-essentials cmake
+sudo apt-get install python-dev
 cd bundle/YouCompleteMe.vim/
-bash ./install.sh --omnisharp-completer
-
+git submodule update --init --recursive
+./install.sh --omnisharp-completer
 
 printf 'Vim configuration complete!'
+
+
