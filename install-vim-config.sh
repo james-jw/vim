0c5d263ff20be555927bf9049e391c9c30fab558
James Wright <james.jw@hotmail.com>
2015-06-15 08:28:43 -0600

    Added airline status bar plugin

--- install-vim-config.sh
+++ install-vim-config.sh
@@ -5,13 +5,13 @@ wget -nv -N https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload
 cd ..
 wget -nv -N https://raw.githubusercontent.com/james-jw/vim/master/vimrc --no-check-certificate
 wget -nv -N https://bitbucket.org/ns9tks/vim-autocomplpop/get/tip.zip --no-check-certificate
-unzip tip.zip -d /bundle/autocomplete.vim
+unzip tip.zip -d bundle/autocomplete.vim
 rm tip.zip
 
 git clone --depth=1 git@github.com:tpope/vim-surround.git bundle/vim-surround.vim
 git clone --depth=1 git@github.com:james-jw/vim-xquery-syntax.git bundle/xquery-syntax.vim
 git clone --depth=1 git@github.com:Townk/vim-autoclose.git bundle/autoclose.vim
-git clone --depth=1 git@github.com:maciakl/vim-neatstatus.git bundle/neatstatus.vim
+git clone --depth=1 git@github.com:bling/vim-airline bundle/airline.vim
 git clone --depth=1 git@github.com:vim-scripts/closetag.vim.git bundle/closetag.vim
 git clone --depth=1 git@github.com:scrooloose/nerdtree.git bundle/nerdtree.vim
 git clone --depth=1 git@github.com:tpope/vim-fugitive.git bundle/fugitive.vim
